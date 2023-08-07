ae950dac9fda4f49e249a9ff69fd08bc6f7fdd06
hyrw <xiehui88i888@163.com>
2023-08-07 21:32:44 +0800

    增加 Plugin Demo

---
+++ PluginDemo.Main/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PluginDemo.Core;
+using System.Reflection;
+
+var dllFiles = Directory.EnumerateFiles(AppContext.BaseDirectory, "*.dll");
+var assemblys = new List<Assembly>();
+foreach (var dllFile in dllFiles)
+{
+    assemblys.Add(Assembly.LoadFile(dllFile));
+}
+
+var pluginTypes = assemblys
+    .SelectMany(i => i.GetExportedTypes())
+    .Where(i => i.IsAssignableTo(typeof(IPlugin)));
+
+foreach (var pluginType in pluginTypes)
+{
+    IPlugin? plugin = Activator.CreateInstance(pluginType) as IPlugin;
+    plugin?.Execute();
+}
