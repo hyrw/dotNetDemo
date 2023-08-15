7768be1c6bc722e84b50c3646bd8c534b4389e49
hyrw <xiehui88i888@163.com>
2023-08-15 23:34:25 +0800

    添加 水波纹特效

---
+++ WaterRipple/WaterRippleButton.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace WaterRipple;
+
+// source http://note.youdao.com/s/ZsJLkpDu
+public partial class WaterRippleButton : Button
+{
+    public WaterRippleButton()
+    {
+        InitializeComponent();
+    }
+
+    private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        var expansionAnimation = new DoubleAnimation()
+        {
+            From = 0,
+            To = 150,
+            Duration = TimeSpan.FromSeconds(1)
+        };
+        var opacityAnimation = new DoubleAnimation()
+        {
+            From = 0.3,
+            To = 0,
+            Duration = TimeSpan.FromSeconds(1)
+        };
+        
+        var ellipseGeometry = Template.FindName("_geometry", this) as EllipseGeometry;
+        var path = Template.FindName("_path", this) as Path;
+        
+        ellipseGeometry!.Center = Mouse.GetPosition(this);
+        ellipseGeometry.BeginAnimation(EllipseGeometry.RadiusXProperty, expansionAnimation);
+        
+        path!.BeginAnimation(Path.OpacityProperty, opacityAnimation);
+    }
+}
