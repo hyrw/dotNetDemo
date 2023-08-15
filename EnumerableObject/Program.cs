6b2be700b34349c1e484f6ed2b32467f446a3085
hyrw <xiehui88i888@163.com>
2023-08-15 14:22:39 +0800

    可枚举对象 Demo

---
+++ EnumerableObject/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+
+var person = new Person("Test", 18);
+foreach (var i in person)
+{
+    Console.WriteLine(i);
+}
+
+public class Person : IEnumerable<string>
+{
+    private string name;
+    private int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public IEnumerator<string> GetEnumerator()
+    {
+        yield return name;
+        yield return age.ToString();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
