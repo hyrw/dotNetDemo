1a0775874914c5ea58c6e6cc564452d56d825cbc
hyrw <xiehui88i888@163.com>
2024-03-20 12:53:02 +0800

    单例的三种实现方式(线程安全)

---
+++ DesignPpattern/Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine();
+
+public class Singleton_0
+{
+    private static readonly Singleton_0 instance = new();
+    public static Singleton_0 Instance => instance;
+
+    private Singleton_0() { }
+}
+
+public class Singleton_1
+{
+    private readonly static object lockObj = new();
+    private static Singleton_1 instance;
+    public static Singleton_1 Instance
+    {
+        get
+        { 
+            if (instance == null)
+            {
+                lock (lockObj)
+                {
+                    instance ??= new Singleton_1();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private Singleton_1() { }
+}
+
+public class Singleton_2
+{
+    private readonly static Lazy<Singleton_2> instance = new();
+    public static Singleton_2 Instance
+    {
+        get => instance.Value;
+    }
+
+    public Singleton_2() { }
+}
