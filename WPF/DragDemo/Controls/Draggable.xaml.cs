3f3ab34ff48dfc382d534e7ce3239194f4891980
hyrw <xiehui88i888@163.com>
2024-06-15 00:03:50 +0800

    Drag&Drop demo

---
+++ WPF/DragDemo/Controls/Draggable.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DragDemo.Controls;
+
+/// <summary>
+/// Draggable.xaml 的交互逻辑
+/// </summary>
+public partial class Draggable : UserControl
+{
+    DraggableAdorner _draggableAdorner;
+    public Draggable()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e)
+    {
+        base.OnMouseMove(e);
+        if (e.LeftButton != MouseButtonState.Pressed) return;
+
+        var data = new DataObject();
+        data.SetData(typeof(Brush), this.Background);
+
+        var adornerLayer = AdornerLayer.GetAdornerLayer(this);
+        _draggableAdorner = new DraggableAdorner(this);
+        adornerLayer.Add(_draggableAdorner);
+
+        DragDrop.DoDragDrop(this, data, DragDropEffects.Copy);
+
+        adornerLayer.Remove(_draggableAdorner);
+
+        e.Handled = true;
+    }
+
+    protected override void OnPreviewGiveFeedback(GiveFeedbackEventArgs e)
+    {
+        // todo: 拖动预览
+        base.OnPreviewGiveFeedback(e);
+        Point point = Mouse.GetPosition(this);
+        _draggableAdorner.Arrange(new Rect(point, _draggableAdorner.RenderSize));
+        Trace.WriteLine($"{DateTime.Now} GiveFeedback point {point}");
+    }
+
+    private class DraggableAdorner : Adorner
+    {
+        readonly Draggable _adornedElement;
+
+        Rect _rect;
+        Brush _brush;
+
+        public DraggableAdorner(Draggable adornedElement) : base(adornedElement)
+        {
+            _adornedElement = adornedElement;
+            _rect = new Rect(_adornedElement.RenderSize);
+            _brush = _adornedElement.Background.Clone();
+            this.IsHitTestVisible = false;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            drawingContext.DrawRectangle(_brush, null, _rect);
+        }
+    }
+}
---
+++ WPF/DragDemo/Controls/DropZone.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragDemo.Controls;
+
+/// <summary>
+/// DropZoon.xaml 的交互逻辑
+/// </summary>
+public partial class DropZoon : UserControl
+{
+    readonly DoubleCollection strokeDashArray;
+    public DropZoon()
+    {
+        InitializeComponent();
+        strokeDashArray = [4, 2];
+    }
+
+    protected override void OnDrop(DragEventArgs e)
+    {
+        base.OnDrop(e);
+        if (e.Data.GetDataPresent(typeof(Brush)))
+        {
+            var brush = (Brush)e.Data.GetData(typeof(Brush));
+            this.borderRect.Fill = brush;
+            this.borderRect.Stroke = Brushes.Black;
+            this.borderRect.StrokeDashArray = null;
+        }
+        e.Handled = true;
+    }
+
+    protected override void OnDragLeave(DragEventArgs e)
+    {
+        base.OnDragLeave(e);
+        this.borderRect.Stroke = Brushes.Black;
+        this.borderRect.StrokeDashArray = null;
+        e.Handled = true;
+    }
+
+    protected override void OnDragEnter(DragEventArgs e)
+    {
+        base.OnDragEnter(e);
+        if (e.Data.GetDataPresent(typeof(Brush)))
+        {
+            var brush = (Brush)e.Data.GetData(typeof(Brush));
+            this.borderRect.Stroke = brush;
+            this.borderRect.StrokeDashArray = strokeDashArray;
+        }
+        e.Handled = true;
+    }
+}
