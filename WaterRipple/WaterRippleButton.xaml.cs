322c61b0150db72ce29d51c282c4f24ccee0b7c5
hyrw <xiehui88i888@163.com>
2024-06-18 16:25:31 +0800

    重构水波纹

--- WaterRipple/WaterRippleButton.xaml.cs
+++ WaterRipple/WaterRippleButton.xaml.cs
@@ -4,38 +4,57 @@ using System.Windows.Input;
 using System.Windows.Shapes;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows;
 
 namespace WaterRipple;
 
 // source http://note.youdao.com/s/ZsJLkpDu
+[TemplatePart(Name = PART_Geometry, Type = typeof(EllipseGeometry))]
+[TemplatePart(Name = PART_Path, Type = typeof(Path))]
 public partial class WaterRippleButton : Button
 {
+    const string PART_Geometry = "PART_Geometry";
+    const string PART_Path = "PART_Path";
+    EllipseGeometry? _ellipseGeometry;
+    Path? _path;
+
+    readonly DoubleAnimation _radius;
+    readonly DoubleAnimation _opacity;
+
     public WaterRippleButton()
     {
         InitializeComponent();
-    }
 
-    private void UIElement_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-        var expansionAnimation = new DoubleAnimation()
+         _radius = new DoubleAnimation()
         {
             From = 0,
             To = 150,
             Duration = TimeSpan.FromSeconds(1)
         };
-        var opacityAnimation = new DoubleAnimation()
+        _opacity = new DoubleAnimation()
         {
             From = 0.3,
             To = 0,
             Duration = TimeSpan.FromSeconds(1)
         };
-        
-        var ellipseGeometry = Template.FindName("_geometry", this) as EllipseGeometry;
-        var path = Template.FindName("_path", this) as Path;
-        
-        ellipseGeometry!.Center = Mouse.GetPosition(this);
-        ellipseGeometry.BeginAnimation(EllipseGeometry.RadiusXProperty, expansionAnimation);
-        
-        path!.BeginAnimation(Path.OpacityProperty, opacityAnimation);
     }
+
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        _ellipseGeometry = Template.FindName(PART_Geometry, this) as EllipseGeometry;
+        _path = Template.FindName(PART_Path, this) as Path;
+    }
+
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        base.OnMouseLeftButtonDown(e);
+
+        _ellipseGeometry!.Center = Mouse.GetPosition(this);
+        _ellipseGeometry.BeginAnimation(EllipseGeometry.RadiusXProperty, _radius);
+        _path!.BeginAnimation(Path.OpacityProperty, _opacity);
+
+        e.Handled = true;
+    }
+
 }
