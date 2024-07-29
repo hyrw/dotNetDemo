ca295f3f39467e16a9d6d1a963ade5adabe79c0f
hyrw <xiehui88i888@163.com>
2024-07-29 16:13:57 +0800

    翻转卡牌（有拉伸问题）

---
+++ WPF/ControlLibrary/CustomControls/FlipCard.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlLibrary.Controls;
+
+public class FlipCard : Control
+{
+    public UIElement Front
+    {
+        get { return (UIElement)GetValue(FrontProperty); }
+        set { SetValue(FrontProperty, value); }
+    }
+    public UIElement Back
+    {
+        get { return (UIElement)GetValue(BackProperty); }
+        set { SetValue(BackProperty, value); }
+    }
+
+    public static readonly DependencyProperty FrontProperty;
+    public static readonly DependencyProperty BackProperty;
+
+    static FlipCard()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(FlipCard), new FrameworkPropertyMetadata(typeof(FlipCard)));
+        FrontProperty = DependencyProperty.Register(nameof(Front), typeof(UIElement), typeof(FlipCard), new PropertyMetadata(default));
+        BackProperty = DependencyProperty.Register(nameof(Back), typeof(UIElement), typeof(FlipCard), new PropertyMetadata(default));
+    }
+}
