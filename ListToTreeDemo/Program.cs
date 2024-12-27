7a0be0003194ff88685e65ca9eb6559d06ef726a
hyrw <xiehui88i888@163.com>
2024-12-27 23:13:51 +0800

    暂存

--- ListToTreeDemo/Program.cs
+++ ListToTreeDemo/Program.cs
@@ -1,133 +1,86 @@
-﻿
-var menuList = new List<ITree<int>>
+﻿using System.Data;
+
+var menuList = new List<Menu>
 {
-    new Menu{ID = 1, ParentID = Menu.INVALID_ID, Name = "视图管理"},
-    new Menu{ID = 11, ParentID = 1, Name = "库位视图"},
-    new Menu{ID = 12, ParentID = 1, Name = "通道视图"},
-    new Menu{ID = 2, ParentID = Menu.INVALID_ID, Name = "流程管理"},
-    new Menu{ID = 21, ParentID = 2, Name = "流程编辑"},
-    new Menu{ID = 3, ParentID = Menu.INVALID_ID, Name = "库位管理"},
+    new() {ID = 1,  Name = "视图管理"},
+    new() {ID = 11, ParentID = 1, Name = "库位视图"},
+    new() {ID = 12, ParentID = 1, Name = "通道视图"},
+    new() {ID = 2, Name = "流程管理"},
+    new() {ID = 21, ParentID = 2, Name = "流程编辑"},
+    new() {ID = 3, Name = "库位管理"},
 };
 
-//var menu = new Menu { ID = 3, ParentID = Menu.INVALID_ID, Name = "库位管理" };
-var menuTree = TreeUtils.GenerateTree(menuList);
-var menuTree2 = TreeUtils.GenerateTreeLambda(menuTree,
-                                             menu => menu.Id,
-                                             menu => menu.ParentId,
-                                             menu => menu.IsRoot,
-                                             menu => menu.Children,
-                                             (menu, list) => menu.Children = list);
+var result = TreeUtils.GenerateTreeLambdaV2(menuList, 
+    e => e.ID, 
+    e => e.ParentID, 
+    e => e.ParentID == default, 
+    treeItem => new Menu() 
+    { 
+        ID = treeItem.Value.ID, 
+        ParentID = treeItem.Value.ParentID, 
+        Children = treeItem.Children, 
+        Name = treeItem.Value.Name 
+    });
+Console.ReadKey();
+
 public class TreeUtils
 {
-    public static List<ITree<T>> GenerateTree<T>(List<ITree<T>> trees) where T : notnull, new()
-    {
-        if (trees is null)
-        {
-            throw new ArgumentNullException();
-        }
-
-        List<ITree<T>> list = new();
-        Dictionary<T, ITree<T>> dict = trees.ToDictionary(item => item.Id);
-
-        foreach (var item in dict)
-        {
-            if (item.Value.IsRoot)
-            {
-                list.Add(item.Value);
-            }
-            else
-            {
-                if (dict.TryGetValue(item.Value.ParentId, out ITree<T>? parentTree))
-                {
-                    parentTree.Children ??= new List<ITree<T>>();
-                    parentTree.Children.Add(item.Value);
-                }
-            }
-        }
-        return list;
-    }
-
-    public static List<T> GenerateTreeLambda<TKey, T>(List<T> trees,
-                                                      Func<T, TKey> getId,
-                                                      Func<T, TKey> getParentId,
+    public static IEnumerable<T> GenerateTreeLambdaV2<TKey, T>(List<T> source,
+                                                      Func<T, TKey> key,
+                                                      Func<T, TKey> parentKey,
                                                       Func<T, bool> isRoot,
-                                                      Func<T, List<T>> getChildren,
-                                                      Action<T, List<T>> setChildren)
+                                                      Func<TreeItem<T>, T> factory)
         where TKey : notnull
-        where T : notnull
     {
-        if (trees is null)
-        {
-            throw new ArgumentNullException();
-        }
+        ArgumentNullException.ThrowIfNull(source);
 
-        List<T> rootTree = new();
-        Dictionary<TKey, T> dict = trees.ToDictionary(getId);
+        List<TreeItem<T>> rootTree = [];
+        Dictionary<TKey, TreeItem<T>> dict = source.ToDictionary(key, e => new TreeItem<T> { Value = e });
 
-        foreach (var item in dict)
+        foreach (var (_, treeItem) in dict)
         {
-            if (isRoot(item.Value))
+            if (isRoot(treeItem.Value))
             {
-                rootTree.Add(item.Value);
+                rootTree.Add(treeItem);
             }
-            else
+            if (dict.TryGetValue(parentKey(treeItem.Value), out var parent))
             {
-                if (dict.TryGetValue(getParentId(item.Value), out T? parentTree))
-                {
-                    List<T>? children = getChildren(parentTree);
-                    if (children == null)
-                    {
-                        children = new List<T>();
-                        setChildren(item.Value, children);
-                    }
-                    children.Add(item.Value);
-                }
+                parent.Children.Add(treeItem.Value);
             }
         }
-        return rootTree;
+
+        return rootTree.Select(factory);
+    }
+
+    public static IEnumerable<T> GenerateTreeLookup<TKey, T>(List<T> source,
+                                                      Func<T, TKey> key,
+                                                      Func<T, TKey> parentKey,
+                                                      Func<T, bool> isRoot,
+                                                      Func<T, IEnumerable<T>, T> factory)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        var lookup = source.ToLookup(e => parentKey(e));
+
+        var result = source.Select(e =>
+        {
+            var children = lookup[key(e)].ToArray();
+            return factory(e, children);
+        }).Where(isRoot);
+        return result;
     }
 }
 
-
-public interface ITree<T> where T : notnull
+public class TreeItem<T>
 {
-    T Id { get; }
-    T ParentId { get; }
-    bool IsRoot { get; }
-
-    List<ITree<T>> Children { get; set; }
-
+    public required T Value { get; set; }
+    public List<T> Children { get; set; } = [];
 }
 
-public class Menu : ITree<int>
+
+public class Menu
 {
-    public void TestFunc()
-    {
-        
-    }
-
-    public static int INVALID_ID = int.MaxValue;
-
-    int ITree<int>.Id => this.ID;
-
-    int ITree<int>.ParentId => this.ParentID;
-
-    bool ITree<int>.IsRoot => this.ParentID == INVALID_ID;
-
-    List<ITree<int>> ITree<int>.Children 
-    { 
-        get => children;
-        set => children = value;
-    }
-
-    public int ID { get; set; } 
+    public int ID { get; set; }
     public int ParentID { get; set; }
-    public string Name { get; set; }
-    private List<ITree<int>> children;
-
-    public Menu()
-    {
-        this.Name = string.Empty;
-        this.children = new List<ITree<int>>();
-    }
+    public string Name { get; set; } = string.Empty;
+    public List<Menu> Children = [];
 }
