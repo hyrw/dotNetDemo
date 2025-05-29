c6587e65ba2f1e63ec58ac4d00e25fbbd124ba10
hyrw <xiehui88i888@163.com>
2025-05-29 18:59:56 +0800

    feat: OpencCv 轮廓层级扩展方法

---
+++ Avalonia/AvaloniaWithOpenCV/Extensions/HierarchyIndexExtensions.cs
@@ -0,0 +1,10 @@
+﻿using OpenCvSharp;
+
+namespace AvaloniaWithOpenCV.Extensions;
+
+public static class HierarchyIndexExtensions
+{
+    public static bool HasParent(this HierarchyIndex h) => h.Parent != -1;
+
+    public static bool HasChild(this HierarchyIndex h) => h.Child != -1;
+}
