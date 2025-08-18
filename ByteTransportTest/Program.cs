7f0ccf4288a820d4367ee832966a8f2c397015bd
hyrw <xiehui88i888@163.com>
2025-08-18 14:41:39 +0800

    demo

--- ByteTransportTest/Program.cs
+++ ByteTransportTest/Program.cs
@@ -1,8 +1,32 @@
 ﻿using ByteTransport;
 using System.Text;
 
-IByteTransport transport = new SerialStreamTransport("COM1", 9600);
-transport.Connect();
-await transport.SendAsync(Encoding.UTF8.GetBytes("hello"));
-var result = await transport.ReceiveAsync();
-Console.WriteLine(Encoding.Default.GetString(result));
+IByteTransport comA = new SerialStreamTransport("COM1", 9600);
+IByteTransport comB = new SerialStreamTransport("COM2", 9600);
+comA.Connect();
+comB.Connect();
+
+var a = Task.Run(async () =>
+{
+    while (true)
+    {
+        string msg = "hello";
+        await comA.SendAsync(Encoding.Default.GetBytes(msg));
+        var bytes = await comA.ReceiveAsync();
+        Console.WriteLine($"COMA: {Encoding.Default.GetString(bytes)}");
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+});
+var b = Task.Run(async () =>
+{
+    while (true)
+    {
+        var bytes = await comB.ReceiveAsync();
+        Console.WriteLine($"COMB: {Encoding.Default.GetString(bytes)}");
+        string msg = "world";
+        await comB.SendAsync(Encoding.Default.GetBytes(msg));
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+});
+
+await Task.WhenAll(a, b);
