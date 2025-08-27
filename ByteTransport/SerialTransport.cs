b5d2972083c87b7773e06bf0c48249e4851eb682
hyrw <xiehui88i888@163.com>
2025-08-27 16:56:54 +0800

    feat: 实现接口

--- ByteTransport/SerialTransport.cs
+++ ByteTransport/SerialTransport.cs
@@ -1,83 +1,150 @@
-//using System.IO.Ports;
+/* using System.IO.Ports;
+using System.Threading.Tasks.Sources;
 
-//namespace ByteTransport;
+namespace ByteTransport;
 
-//public class SerialTransport(string com, int baudRate) : IByteTransport
-//{
-//    SerialPort? serialPort;
+public class MyValueTaskSource : IValueTaskSource<short>
+{
+    ManualResetValueTaskSourceCore<short> core = new();
+    public short GetResult(short token) => core.GetResult(token);
 
-//    public void Connect()
-//    {
-//        serialPort ??= new SerialPort(com, baudRate);
-//        serialPort.Open();
-//    }
+    public ValueTaskSourceStatus GetStatus(short token)
+    {
+        return core.GetStatus(token);
+    }
 
-//    public void Disconnect()
-//    {
-//        if (serialPort is null) return;
+    public void OnCompleted(Action<object?> continuation, object? state, short token, ValueTaskSourceOnCompletedFlags flags) => core.OnCompleted(continuation, state, token, flags);
+}
 
-//        serialPort.Close();
-//        serialPort.Dispose();
-//        serialPort = null;
-//    }
+public class SerialTransport(string com, int baudRate) : IByteTransport
+{
+    SerialPort? serialPort;
 
-//    public bool IsConnected()
-//    {
-//        if (serialPort is null) return false;
-//        return serialPort.IsOpen;
-//    }
+    bool IByteTransport.IsConnected => throw new NotImplementedException();
 
-//    public async Task<byte[]> ReceiveAsync(CancellationToken token = default)
-//    {
-//        if (serialPort is null || !serialPort.IsOpen)
-//            throw new InvalidOperationException("Serial port is not connected");
+    public Task ConnectAsync(CancellationToken token = default)
+    {
+        token.ThrowIfCancellationRequested();
 
-//        token.ThrowIfCancellationRequested();
+        serialPort ??= new SerialPort(com, baudRate);
+        serialPort.Open();
+        return Task.CompletedTask;
+    }
 
-//        var taskSource = new TaskCompletionSource<byte[]>();
-//        using var registration = token.Register(() => taskSource.TrySetCanceled());
+    public Task DisconnectAsync(CancellationToken token = default)
+    {
+        if (serialPort is null) return Task.CompletedTask;
 
-//        void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-//        {
-//            try
-//            {
-//                if (serialPort.BytesToRead > 0)
-//                {
-//                    int bytesToRead = serialPort.BytesToRead;
-//                    byte[] buffer = new byte[bytesToRead];
-//                    int bytesRead = serialPort.Read(buffer, 0, bytesToRead);
-//                    taskSource.TrySetResult(bytesRead == bytesToRead ? buffer : buffer[..bytesRead]);
-//                }
-//            }
-//            catch (Exception ex)
-//            {
-//                taskSource.TrySetException(ex);
-//            }
-//        }
+        token.ThrowIfCancellationRequested();
 
-//        serialPort.DataReceived += DataReceivedHandler;
-//        try
-//        {
-//            return await taskSource.Task;
-//        }
-//        finally
-//        {
-//            serialPort.DataReceived -= DataReceivedHandler;
-//        }
-//    }
+        serialPort.Close();
+        serialPort.Dispose();
+        serialPort = null;
+        return Task.CompletedTask;
+    }
 
-//    public Task SendAsync(byte[] data, CancellationToken token = default)
-//    {
-//        if (serialPort is null || !serialPort.IsOpen)
-//            throw new InvalidOperationException("Serial port is not connected");
-//        ArgumentNullException.ThrowIfNull(data);
+    public bool IsConnected()
+    {
+        if (serialPort is null) return false;
+        return serialPort.IsOpen;
+    }
 
-//        token.ThrowIfCancellationRequested();
+    public async ValueTask<int> ReceiveAsync(Memory<byte> buffer, CancellationToken token = default)
+    {
+        if (serialPort is null || !serialPort.IsOpen)
+            throw new InvalidOperationException("Serial port is not connected");
 
-//        return Task.Run(() =>
-//        {
-//            token.ThrowIfCancellationRequested();
-//            serialPort.Write(data, 0, data.Length);
-//        }, token);
-//    }
-//}
+        token.ThrowIfCancellationRequested();
+
+        var taskSource = new TaskCompletionSource<int>();
+        using var registration = token.Register(() => taskSource.TrySetCanceled());
+
+        async void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (serialPort!.BytesToRead > 0)
+                {
+                    int bytesRead = await serialPort.BaseStream.ReadAsync(buffer, token);
+                    taskSource.TrySetResult(bytesRead);
+                }
+            }
+            catch (Exception ex)
+            {
+                taskSource.TrySetException(ex);
+            }
+        }
+
+        serialPort.DataReceived += DataReceivedHandler;
+        try
+        {
+            return await taskSource.Task;
+        }
+        finally
+        {
+            serialPort.DataReceived -= DataReceivedHandler;
+        }
+    }
+
+    public async ValueTask<int> ReceiveAsync(byte[] buffer, int offset, int count, CancellationToken token = default)
+    {
+        if (serialPort is null || !serialPort.IsOpen)
+            throw new InvalidOperationException("Serial port is not connected");
+
+        token.ThrowIfCancellationRequested();
+
+        var taskSource = new TaskCompletionSource<int>();
+        using var registration = token.Register(() => taskSource.TrySetCanceled());
+
+        void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (serialPort.BytesToRead > 0)
+                {
+                    int bytesRead = serialPort.Read(buffer, offset, count);
+                    taskSource.TrySetResult(bytesRead);
+                }
+            }
+            catch (Exception ex)
+            {
+                taskSource.TrySetException(ex);
+            }
+        }
+
+        serialPort.DataReceived += DataReceivedHandler;
+        try
+        {
+            return await taskSource.Task;
+        }
+        finally
+        {
+            serialPort.DataReceived -= DataReceivedHandler;
+        }
+    }
+
+    public ValueTask SendAsync(ReadOnlyMemory<byte> buffer, CancellationToken token = default)
+    {
+        if (serialPort is null || !serialPort.IsOpen)
+            throw new InvalidOperationException("Serial port is not connected");
+        ArgumentNullException.ThrowIfNull(buffer);
+
+        token.ThrowIfCancellationRequested();
+        return serialPort.BaseStream.WriteAsync(buffer, token);
+    }
+
+    public async ValueTask SendAsync(byte[] buffer, int offset, int count, CancellationToken token = default)
+    {
+        if (serialPort is null || !serialPort.IsOpen)
+            throw new InvalidOperationException("Serial port is not connected");
+        ArgumentNullException.ThrowIfNull(buffer);
+
+        token.ThrowIfCancellationRequested();
+
+        await Task.Run(() =>
+        {
+            token.ThrowIfCancellationRequested();
+            serialPort.Write(buffer, offset, count);
+        }, token);
+    }
+} */
