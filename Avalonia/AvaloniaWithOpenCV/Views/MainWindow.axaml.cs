76d9f18cdc6439d45472328f39625a6446fa745f
hyrw <xiehui88i888@gmail.com>
2026-03-16 20:06:54 +0800

    feat: Avalonia MatrixTransform Animation

--- Avalonia/AvaloniaWithOpenCV/Views/MainWindow.axaml.cs
+++ Avalonia/AvaloniaWithOpenCV/Views/MainWindow.axaml.cs
@@ -1,6 +1,6 @@
 using Avalonia;
 using Avalonia.Input;
-using Avalonia.Media;
+using Avalonia.Media.Transformation;
 using AvaloniaWithOpenCV.ViewModels;
 using OpenCvSharp;
 using System.IO;
@@ -16,15 +16,18 @@ public partial class MainWindow : Avalonia.Controls.Window
 
     private void OnPointerWheelChanged(object? sender, PointerWheelEventArgs e)
     {
-        if (this.EleContainer.RenderTransform is not MatrixTransform matrixTransform) return;
+        if (this.EleContainer.RenderTransform is null) return;
         e.Handled = true;
 
         var p = e.GetCurrentPoint(this.EleContainer);
         double scale = e.Delta.Y > 0 ? 1.2 : 1 / 1.2;
 
-        var m = matrixTransform.Value;
+        var m = this.EleContainer.RenderTransform.Value;
+        m = m.ScaleAtPrepend(scale, scale, p.Position.X, p.Position.Y);
 
-        matrixTransform.Matrix = m.ScaleAtPrepend(scale, scale, p.Position.X, p.Position.Y);
+        // 不用动画可以直接用MatrixTransform
+        var t = TransformOperations.Parse($"matrix({m.M11},{m.M21},{m.M21},{m.M22},{m.M31},{m.M32})");
+        this.EleContainer.RenderTransform = t;
     }
 
     public Mat? Mat { get; set; }
